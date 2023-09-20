e486be25b5167cc820c2c3e6e9f9613a4826c40f
hanifstudio <hanifyaskur@gmail.com>
2023-09-20 14:10:23 +0700

    feat(ui): implement homepage following wireframe design
    - Follow docs/wireframes/homepage.html exactly
    - Top bar with shipping promo
    - Navigation with mobile menu button
    - Hero section with 2-column grid
    - Category strip with circular images
    - Featured products grid with badges (Baru/Sale)
    - Split banner for Freediving/Scuba collections
    - Community #ArnesDive UGC section
    - Value props (Gratis Ongkir, Original, Return, Support)
    - Footer with newsletter, shopping, help, about links
    - Use brand colors (#333333, #666666, #999999)
    - Indonesian language throughout

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,35 +1,103 @@
+import Link from 'next/link';
+
 export default function StoreLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 items-center justify-between">
-          <a href="/" className="font-bold text-xl">
-            Arne&apos;s Dive Shop
-          </a>
-          <nav className="flex items-center gap-6">
-            <a href="/products" className="text-sm font-medium hover:underline">
-              Products
-            </a>
-            <a href="/categories" className="text-sm font-medium hover:underline">
-              Categories
-            </a>
-            <a href="/cart" className="text-sm font-medium hover:underline">
-              Cart
-            </a>
-            <a href="/login" className="text-sm font-medium hover:underline">
-              Login
-            </a>
-          </nav>
+    <div className="min-h-screen flex flex-col font-sans text-sm leading-relaxed">
+      {/* Top Bar */}
+      <div className="bg-gray-200 py-2 text-center text-xs">
+        Gratis Ongkir untuk pembelian di atas Rp 500.000
+      </div>
+
+      {/* Navigation */}
+      <nav className="flex items-center justify-between px-4 py-4 border-b border-gray-200 max-w-7xl mx-auto w-full">
+        {/* Mobile Menu Button */}
+        <button
+          className="lg:hidden text-gray-600"
+          type="button"
+          aria-label="Menu"
+        >
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+
+        {/* Desktop Nav Links */}
+        <div className="hidden lg:flex gap-6">
+          <Link href="/" className="text-gray-600 hover:text-brand text-sm">Beranda</Link>
+          <Link href="/freediving" className="text-gray-600 hover:text-brand text-sm">Freediving</Link>
+          <Link href="/scuba" className="text-gray-600 hover:text-brand text-sm">Scuba</Link>
+          <Link href="/aksesoris" className="text-gray-600 hover:text-brand text-sm">Aksesoris</Link>
+          <Link href="/sale" className="text-red-500 hover:text-red-700 text-sm">Sale</Link>
         </div>
-      </header>
+
+        {/* Logo */}
+        <Link
+          href="/"
+          className="text-2xl font-bold text-brand absolute left-1/2 -translate-x-1/2 lg:static lg:translate-x-0"
+        >
+          ARNES DIVE
+        </Link>
+
+        {/* Actions */}
+        <div className="flex gap-4 text-gray-600">
+          <Link href="/search" className="hidden sm:block hover:text-brand text-sm">[Cari]</Link>
+          <Link href="/wishlist" className="hover:text-brand text-sm">[Wishlist]</Link>
+          <Link href="/cart" className="hover:text-brand text-sm">[Keranjang]</Link>
+          <Link href="/account" className="hidden sm:block hover:text-brand text-sm">[Akun]</Link>
+        </div>
+      </nav>
+
+      {/* Main Content */}
       <main className="flex-1">{children}</main>
-      <footer className="border-t py-6">
-        <div className="container text-center text-sm text-muted-foreground">
-          © {new Date().getFullYear()} Arne&apos;s Dive Shop. All rights reserved.
+
+      {/* Footer */}
+      <footer className="border-t border-gray-200 py-12 px-4">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div>
+            <h5 className="text-xs uppercase tracking-wider font-semibold mb-4">Newsletter</h5>
+            <p className="text-xs text-gray-600 mb-3">Dapatkan info produk terbaru</p>
+            <input
+              type="email"
+              placeholder="Email Anda"
+              className="w-full px-3 py-2 border border-gray-300 mb-2 text-sm"
+            />
+            <button className="w-full bg-brand text-white px-3 py-2 text-xs uppercase hover:bg-gray-700">
+              Langganan
+            </button>
+          </div>
+          <div>
+            <h5 className="text-xs uppercase tracking-wider font-semibold mb-4">Belanja</h5>
+            <ul className="space-y-2 text-sm">
+              <li><Link href="/freediving" className="text-gray-600 hover:text-brand">Freediving</Link></li>
+              <li><Link href="/scuba" className="text-gray-600 hover:text-brand">Scuba</Link></li>
+              <li><Link href="/aksesoris" className="text-gray-600 hover:text-brand">Aksesoris</Link></li>
+              <li><Link href="/sale" className="text-gray-600 hover:text-brand">Sale</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="text-xs uppercase tracking-wider font-semibold mb-4">Bantuan</h5>
+            <ul className="space-y-2 text-sm">
+              <li><Link href="/faq" className="text-gray-600 hover:text-brand">FAQ</Link></li>
+              <li><Link href="/faq" className="text-gray-600 hover:text-brand">Pengiriman</Link></li>
+              <li><Link href="/faq" className="text-gray-600 hover:text-brand">Pengembalian</Link></li>
+              <li><Link href="/kontak" className="text-gray-600 hover:text-brand">Hubungi Kami</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="text-xs uppercase tracking-wider font-semibold mb-4">Tentang</h5>
+            <ul className="space-y-2 text-sm">
+              <li><Link href="/tentang" className="text-gray-600 hover:text-brand">Tentang Kami</Link></li>
+              <li><Link href="/privasi" className="text-gray-600 hover:text-brand">Kebijakan Privasi</Link></li>
+              <li><Link href="/syarat" className="text-gray-600 hover:text-brand">Syarat & Ketentuan</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-center pt-8 mt-8 border-t border-gray-200 text-xs text-gray-600">
+          © {new Date().getFullYear()} Arnes Dive Shop. All rights reserved.
         </div>
       </footer>
     </div>
--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,32 +1,177 @@
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function HomePage() {
+  const categories = [
+    { name: 'Masker', href: '/freediving?kategori=masker' },
+    { name: 'Fin', href: '/freediving?kategori=fin' },
+    { name: 'Wetsuit', href: '/freediving?kategori=wetsuit' },
+    { name: 'Sabuk', href: '/freediving?kategori=sabuk' },
+    { name: 'Aksesoris', href: '/aksesoris' },
+  ];
+
+  const featuredProducts = [
+    { name: 'Masker Freediving Pro', price: 'Rp 850.000', badge: 'Baru', badgeColor: 'bg-brand' },
+    { name: 'Fin Carbón Pro', price: 'Rp 2.500.000', badge: 'Baru', badgeColor: 'bg-brand' },
+    { name: 'Wetsuit 3mm Premium', price: 'Rp 1.800.000', badge: null, badgeColor: null },
+    { name: 'Snorkel Dry Top', price: 'Rp 250.000', originalPrice: 'Rp 320.000', badge: 'Sale', badgeColor: 'bg-red-500' },
+  ];
+
   return (
-    <div className="container py-12">
-      <section className="text-center mb-12">
-        <h1 className="text-4xl font-bold tracking-tight mb-4">
-          Welcome to Arne&apos;s Dive Shop
-        </h1>
-        <p className="text-xl text-muted-foreground mb-8">
-          Premium diving equipment and gear for underwater enthusiasts
-        </p>
-        <Button size="lg">Shop Now</Button>
+    <>
+      {/* Hero Section */}
+      <section className="grid lg:grid-cols-2 min-h-[400px] lg:min-h-[500px]">
+        <div className="p-8 lg:p-12 flex flex-col justify-center order-2 lg:order-1">
+          <span className="text-xs uppercase tracking-widest text-gray-500 mb-2">Koleksi Terbaru</span>
+          <h1 className="text-3xl lg:text-5xl font-semibold mb-4">
+            Peralatan Freediving Premium
+          </h1>
+          <p className="text-gray-600 mb-6 max-w-md">
+            Temukan masker, fin, dan aksesoris freediving berkualitas tinggi untuk pengalaman diving terbaik Anda.
+          </p>
+          <div className="flex flex-wrap gap-4">
+            <Link
+              href="/freediving"
+              className="bg-brand text-white px-6 py-3 text-xs uppercase tracking-wider hover:bg-gray-700"
+            >
+              Lihat Koleksi
+            </Link>
+            <Link
+              href="/sale"
+              className="border border-brand px-6 py-3 text-xs uppercase tracking-wider hover:bg-gray-100"
+            >
+              Sale
+            </Link>
+          </div>
+        </div>
+        <div className="bg-gray-100 border-2 border-dashed border-gray-300 flex items-center justify-center text-gray-400 min-h-[250px] lg:min-h-full order-1 lg:order-2">
+          Hero Image
+        </div>
       </section>
 
-      <section className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
-        {[1, 2, 3, 4].map((i) => (
-          <div
-            key={i}
-            className="rounded-lg border bg-card p-6 shadow-sm"
-          >
-            <div className="aspect-square bg-muted rounded-md mb-4" />
-            <h3 className="font-semibold mb-2">Product {i}</h3>
-            <p className="text-sm text-muted-foreground">
-              Rp {(i * 500000).toLocaleString('id-ID')}
-            </p>
-          </div>
-        ))}
+      {/* Category Strip */}
+      <section className="py-8 border-b border-gray-200">
+        <div className="flex justify-center gap-6 lg:gap-8 px-4 overflow-x-auto">
+          {categories.map((category) => (
+            <Link
+              key={category.name}
+              href={category.href}
+              className="flex flex-col items-center gap-2 flex-shrink-0"
+            >
+              <div className="w-16 h-16 lg:w-20 lg:h-20 rounded-full bg-gray-100 border-2 border-dashed border-gray-300 flex items-center justify-center text-gray-400 text-xs">
+                Img
+              </div>
+              <span className="text-xs lg:text-sm whitespace-nowrap">{category.name}</span>
+            </Link>
+          ))}
+        </div>
       </section>
-    </div>
+
+      {/* Products Section */}
+      <section className="py-12 px-4 max-w-7xl mx-auto">
+        <div className="text-center mb-8">
+          <h2 className="text-xl lg:text-2xl font-semibold">Produk Unggulan</h2>
+          <p className="text-gray-600 text-sm">Pilihan terbaik untuk freediving dan scuba diving</p>
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-6">
+          {featuredProducts.map((product) => (
+            <Link key={product.name} href="/produk/contoh" className="group">
+              <div className="aspect-[3/4] bg-gray-100 border-2 border-dashed border-gray-300 flex items-center justify-center text-gray-400 mb-2 relative">
+                {product.badge && (
+                  <span className={`absolute top-2 left-2 ${product.badgeColor} text-white text-[10px] uppercase px-2 py-1`}>
+                    {product.badge}
+                  </span>
+                )}
+                Product Image
+              </div>
+              <h3 className="font-medium text-sm">{product.name}</h3>
+              {product.originalPrice ? (
+                <p className="text-sm">
+                  <span className="text-red-500">{product.price}</span>{' '}
+                  <s className="text-gray-400">{product.originalPrice}</s>
+                </p>
+              ) : (
+                <p className="text-gray-600 text-sm">{product.price}</p>
+              )}
+            </Link>
+          ))}
+        </div>
+
+        <div className="text-center mt-8">
+          <Link href="/freediving" className="text-sm uppercase border-b border-brand">
+            Lihat Semua →
+          </Link>
+        </div>
+      </section>
+
+      {/* Split Banner */}
+      <section className="grid md:grid-cols-2">
+        <div className="bg-gray-100 border-2 border-dashed border-gray-300 min-h-[300px] lg:min-h-[400px] relative flex items-end p-6 lg:p-8">
+          <div>
+            <span className="text-xs uppercase tracking-widest text-gray-500">Freediving</span>
+            <h3 className="text-2xl lg:text-3xl font-semibold my-2">Koleksi Freediving</h3>
+            <Link href="/freediving" className="inline-block border border-brand px-4 py-2 text-xs uppercase hover:bg-white">
+              Lihat Koleksi
+            </Link>
+          </div>
+        </div>
+        <div className="bg-gray-100 border-2 border-dashed border-gray-300 min-h-[300px] lg:min-h-[400px] relative flex items-end p-6 lg:p-8">
+          <div>
+            <span className="text-xs uppercase tracking-widest text-gray-500">Scuba</span>
+            <h3 className="text-2xl lg:text-3xl font-semibold my-2">Koleksi Scuba</h3>
+            <Link href="/scuba" className="inline-block border border-brand px-4 py-2 text-xs uppercase hover:bg-white">
+              Lihat Koleksi
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      {/* Community */}
+      <section className="py-12 px-4 max-w-7xl mx-auto">
+        <div className="flex justify-between items-end mb-6">
+          <div>
+            <h2 className="text-xl font-semibold">#ArnesDive</h2>
+            <p className="text-gray-600 text-sm">Bagikan petualangan diving Anda</p>
+          </div>
+          <span className="text-gray-600 text-sm hidden sm:block">@arnesdive</span>
+        </div>
+        <div className="grid grid-cols-3 md:grid-cols-5 gap-2">
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div
+              key={i}
+              className={`aspect-square bg-gray-100 border-2 border-dashed border-gray-300 flex items-center justify-center text-gray-400 text-xs ${i > 3 ? 'hidden md:flex' : ''}`}
+            >
+              UGC {i}
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Value Props */}
+      <section className="bg-gray-100 py-12 px-4">
+        <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-6 text-center">
+          <div>
+            <div className="text-3xl mb-2">🚚</div>
+            <h4 className="font-semibold text-sm">Gratis Ongkir</h4>
+            <p className="text-xs text-gray-600">Untuk pembelian di atas Rp 500.000</p>
+          </div>
+          <div>
+            <div className="text-3xl mb-2">✓</div>
+            <h4 className="font-semibold text-sm">Produk Original</h4>
+            <p className="text-xs text-gray-600">100% produk berkualitas</p>
+          </div>
+          <div>
+            <div className="text-3xl mb-2">↩️</div>
+            <h4 className="font-semibold text-sm">Easy Return</h4>
+            <p className="text-xs text-gray-600">14 hari pengembalian</p>
+          </div>
+          <div>
+            <div className="text-3xl mb-2">💬</div>
+            <h4 className="font-semibold text-sm">Support 24/7</h4>
+            <p className="text-xs text-gray-600">Siap membantu kapan saja</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }

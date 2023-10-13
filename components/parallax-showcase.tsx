ece70deb69e57606a52cc0327eebb75615766287
hanifstudio <hanifyaskur@gmail.com>
2023-10-13 13:15:03 +0700

    feat: add cart functionality to product card quick add button

--- components/parallax-showcase.tsx
+++ components/parallax-showcase.tsx
@@ -3,30 +3,29 @@
 import { useRef } from 'react';
 import { motion, useScroll, useTransform, type MotionValue } from 'framer-motion';
 
-// Placeholder gear photography — swap `src`/`objectPosition` for real
-// freediving/scuba cutouts (masks, fins, tanks, regulators) once that
-// photography is available. `objectPosition` crops a different focal
-// area of each shared source photo so repeats don't read as identical.
+// Placeholder gear photography — swap `src` for real freediving/scuba
+// cutouts (masks, fins, tanks, regulators) once that photography is
+// available. Full images shown uncropped, rotated diagonally, scattered
+// edge-to-edge across the section.
 interface FloatingItem {
   src: string;
-  objectPosition: string;
   className: string;
   speed: number;
   rotate: number;
-  flip?: boolean;
 }
 
 const floatingItems: FloatingItem[] = [
-  { src: '/product-sample-1.webp', objectPosition: '50% 15%', className: '-top-[6%] left-[4%] w-44 lg:w-72', speed: -90, rotate: -14 },
-  { src: '/product-sample-2.webp', objectPosition: '50% 55%', className: 'top-[10%] left-[38%] w-32 lg:w-48', speed: 70, rotate: 8 },
-  { src: '/product-sample-1.webp', objectPosition: '80% 40%', className: '-top-[4%] right-[16%] w-36 lg:w-56', speed: 120, rotate: -18, flip: true },
-  { src: '/product-sample-2.webp', objectPosition: '50% 10%', className: 'top-[6%] -right-[4%] w-48 lg:w-80', speed: -75, rotate: 10 },
-  { src: '/product-sample-1.webp', objectPosition: '25% 90%', className: 'top-[42%] left-[14%] w-28 lg:w-40', speed: 60, rotate: 6 },
-  { src: '/product-sample-2.webp', objectPosition: '50% 45%', className: 'top-[46%] right-[36%] w-24 lg:w-36', speed: -100, rotate: -10, flip: true },
-  { src: '/product-sample-1.webp', objectPosition: '50% 10%', className: 'bottom-[4%] left-[8%] w-40 lg:w-60', speed: 85, rotate: 9 },
-  { src: '/product-sample-2.webp', objectPosition: '20% 85%', className: 'bottom-[6%] left-[44%] w-28 lg:w-40', speed: -60, rotate: -7 },
-  { src: '/product-sample-1.webp', objectPosition: '50% 50%', className: '-bottom-[6%] right-[24%] w-32 lg:w-48', speed: 95, rotate: 14 },
-  { src: '/product-sample-2.webp', objectPosition: '75% 35%', className: '-bottom-[8%] -right-[4%] w-44 lg:w-72', speed: -110, rotate: -12, flip: true },
+  { src: '/product-sample-1.webp', className: '-top-[8%] -left-[4%] w-56 lg:w-80', speed: -90, rotate: -35 },
+  { src: '/product-sample-2.webp', className: '-top-[4%] left-[32%] w-48 lg:w-64', speed: 70, rotate: 28 },
+  { src: '/product-sample-1.webp', className: 'top-[2%] right-[18%] w-56 lg:w-80', speed: 110, rotate: -40 },
+  { src: '/product-sample-2.webp', className: '-top-[10%] -right-[6%] w-60 lg:w-96', speed: -75, rotate: 34 },
+  { src: '/product-sample-2.webp', className: 'top-[40%] -left-[6%] w-48 lg:w-72', speed: 60, rotate: 30 },
+  { src: '/product-sample-1.webp', className: 'top-[42%] left-[28%] w-40 lg:w-56', speed: -100, rotate: -25 },
+  { src: '/product-sample-2.webp', className: 'top-[38%] right-[8%] w-44 lg:w-64', speed: 90, rotate: 40 },
+  { src: '/product-sample-1.webp', className: '-bottom-[8%] -left-[4%] w-56 lg:w-80', speed: 85, rotate: 32 },
+  { src: '/product-sample-2.webp', className: 'bottom-[2%] left-[38%] w-44 lg:w-64', speed: -60, rotate: -30 },
+  { src: '/product-sample-1.webp', className: '-bottom-[10%] right-[14%] w-52 lg:w-72', speed: 95, rotate: 38 },
+  { src: '/product-sample-2.webp', className: '-bottom-[6%] -right-[6%] w-60 lg:w-96', speed: -110, rotate: -34 },
 ];
 
 function FloatingObject({
@@ -39,27 +38,13 @@ function FloatingObject({
   const y = useTransform(progress, [0, 1], [0, item.speed]);
 
   return (
-    <motion.div
-      style={{
-        y,
-        rotate: item.rotate,
-        maskImage: 'radial-gradient(closest-side, black 45%, transparent 90%)',
-        WebkitMaskImage: 'radial-gradient(closest-side, black 45%, transparent 90%)',
-      }}
-      className={`absolute aspect-square overflow-hidden pointer-events-none select-none ${item.className}`}
-    >
-      <img
-        src={item.src}
-        alt=""
-        aria-hidden="true"
-        style={{
-          objectPosition: item.objectPosition,
-          transform: item.flip ? 'scaleX(-1)' : undefined,
-          filter: 'grayscale(0.4) brightness(0.55)',
-        }}
-        className="w-full h-full object-cover scale-150"
-      />
-    </motion.div>
+    <motion.img
+      src={item.src}
+      alt=""
+      aria-hidden="true"
+      style={{ y, rotate: item.rotate, filter: 'brightness(0.7)' }}
+      className={`absolute object-contain opacity-70 pointer-events-none select-none ${item.className}`}
+    />
   );
 }
 

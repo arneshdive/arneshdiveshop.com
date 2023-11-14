08b98f31b30296e7e9c1e5a426a87802e86015f0
hanifstudio <hanifyaskur@gmail.com>
2023-11-14 12:00:00 +0700

    feat(auth): add session check API endpoint

---
+++ app/api/auth/session/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { getSession } from '@/lib/auth/session';
+import { db, users } from '@/lib/db';
+import { eq } from 'drizzle-orm';
+
+export async function GET() {
+  try {
+    const session = await getSession();
+
+    if (!session) {
+      return NextResponse.json({ user: null }, { status: 401 });
+    }
+
+    // Fetch fresh user data
+    const user = await db.query.users.findFirst({
+      where: eq(users.id, session.userId),
+    });
+
+    if (!user) {
+      return NextResponse.json({ user: null }, { status: 401 });
+    }
+
+    const { password: _, ...userWithoutPassword } = user;
+    return NextResponse.json({ user: userWithoutPassword });
+  } catch (error) {
+    console.error('Session error:', error);
+    return NextResponse.json({ user: null }, { status: 401 });
+  }
+}
--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,78 @@
-import { auth } from '@/lib/auth/config';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { verifySession, type SessionPayload } from '@/lib/auth/session';
 
-export default auth((req) => {
-  const { nextUrl, auth: session } = req;
-  const isAdmin = nextUrl.pathname.startsWith('/admin');
-  const isAuthPage =
-    nextUrl.pathname.startsWith('/login') ||
-    nextUrl.pathname.startsWith('/register');
-  const isAccountPage = nextUrl.pathname.startsWith('/account');
+// Routes that require authentication
+const protectedRoutes = ['/account'];
 
-  if (isAdmin && !session?.user) {
-    return NextResponse.redirect(new URL('/login', nextUrl));
+// Routes that require admin role
+const adminRoutes = ['/admin'];
+const adminApiRoutes = ['/api/admin'];
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // Get session from cookie
+  const token = request.cookies.get('session')?.value;
+  let session: SessionPayload | null = null;
+
+  if (token) {
+    session = await verifySession(token);
   }
 
-  if (isAdmin && (session?.user as { role?: string })?.role === 'customer') {
-    return NextResponse.redirect(new URL('/', nextUrl));
+  // Check if route requires auth
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+  const isAdminRoute = adminRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+  const isAdminApiRoute = adminApiRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  // No session on protected route → redirect to login
+  if ((isProtectedRoute || isAdminRoute || isAdminApiRoute) && !session) {
+    if (pathname.startsWith('/api/')) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    const loginUrl = new URL('/auth', request.url);
+    loginUrl.searchParams.set('redirect', pathname);
+    return NextResponse.redirect(loginUrl);
   }
 
-  if ((isAuthPage || isAccountPage) && session?.user) {
-    return NextResponse.redirect(new URL('/account', nextUrl));
+  // Admin route but not admin role → deny access
+  if ((isAdminRoute || isAdminApiRoute) && session) {
+    if (session.role !== 'admin' && session.role !== 'super_admin') {
+      if (pathname.startsWith('/api/')) {
+        return NextResponse.json(
+          { error: 'Akses ditolak' },
+          { status: 403 }
+        );
+      }
+      const homeUrl = new URL('/', request.url);
+      return NextResponse.redirect(homeUrl);
+    }
   }
 
-  return NextResponse.next();
-});
+  // Add session to headers for downstream use
+  const response = NextResponse.next();
+  if (session) {
+    response.headers.set('x-user-id', session.userId);
+    response.headers.set('x-user-role', session.role);
+  }
+
+  return response;
+}
 
 export const config = {
-  matcher: ['/admin/:path*', '/login', '/register', '/account/:path*'],
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+  ],
 };

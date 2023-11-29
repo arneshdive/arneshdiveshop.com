f752256f93b20e2704dec2726725803bd7697bc1
hanifstudio <hanifyaskur@gmail.com>
2023-11-29 12:00:00 +0700

    feat(admin): wire products list page to API with React Query

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -4,19 +4,75 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { Icon } from '@iconify/react';
 import { Plus } from 'lucide-react';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { AnimatedButton } from '@/components/ui/animated-button';
 import { Select } from '@/components/admin/input';
-import { categories, brands, diveTypes } from '@/lib/constants/product-options';
-import { featuredProducts as products } from '@/lib/data/mock-products';
+import { categories, brands } from '@/lib/constants/product-options';
 
-// Filter state type
+// Types
+type Product = {
+  id: string;
+  name: string;
+  slug: string;
+  sku: string | null;
+  description: string | null;
+  priceCents: number;
+  compareAtPriceCents: number | null;
+  categoryId: string;
+  brandId: string | null;
+  images: string[];
+  isActive: boolean;
+  isFeatured: boolean;
+  createdAt: string;
+  updatedAt: string;
+  category: { id: string; name: string } | null;
+  brand: { id: string; name: string } | null;
+};
+
 type FilterState = {
   category: string;
   brand: string;
-  diveType: string;
   status: string;
-  stock: string;
 };
+
+// Format price from cents to Rupiah
+function formatPrice(cents: number): string {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  }).format(cents / 100);
+}
+
+// Fetch products from API
+async function fetchProducts(filters: FilterState): Promise<{ products: Product[] }> {
+  const params = new URLSearchParams();
+  if (filters.category) params.set('category', filters.category);
+  if (filters.brand) params.set('brand', filters.brand);
+  if (filters.status) params.set('isActive', filters.status);
+
+  const response = await fetch(`/api/products?${params.toString()}`);
+  if (!response.ok) throw new Error('Failed to fetch products');
+  return response.json();
+}
+
+// Delete product mutation
+async function deleteProduct(id: string): Promise<void> {
+  const response = await fetch(`/api/products/${id}`, {
+    method: 'DELETE',
+  });
+  if (!response.ok) throw new Error('Failed to delete product');
+}
+
+// Toggle product status mutation
+async function toggleProduct(id: string, field: 'isActive' | 'isFeatured', value: boolean): Promise<void> {
+  const response = await fetch(`/api/products/${id}/toggle`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ field, value }),
+  });
+  if (!response.ok) throw new Error('Failed to toggle product');
+}
 
 function getLabelById(items: { id: string; name: string }[], id: string | undefined): string {
   if (!id) return '-';
@@ -24,19 +80,54 @@ function getLabelById(items: { id: string; name: string }[], id: string | undefi
 }
 
 export default function ProductsPage() {
+  const queryClient = useQueryClient();
+  
   const [filters, setFilters] = useState<FilterState>({
     category: '',
     brand: '',
-    diveType: '',
     status: '',
-    stock: '',
+  });
+
+  // Fetch products
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['products', filters],
+    queryFn: () => fetchProducts(filters),
+  });
+
+  // Delete mutation
+  const deleteMutation = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
+  });
+
+  // Toggle mutation
+  const toggleMutation = useMutation({
+    mutationFn: ({ id, field, value }: { id: string; field: 'isActive' | 'isFeatured'; value: boolean }) =>
+      toggleProduct(id, field, value),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
   });
 
   const activeFilterCount = Object.values(filters).filter(v => v !== '').length;
 
   const clearFilters = () => {
-    setFilters({ category: '', brand: '', diveType: '', status: '', stock: '' });
+    setFilters({ category: '', brand: '', status: '' });
   };
+
+  const handleDelete = async (id: string) => {
+    if (confirm('Apakah Anda yakin ingin menghapus produk ini?')) {
+      deleteMutation.mutate(id);
+    }
+  };
+
+  const handleToggleActive = (id: string, currentValue: boolean) => {
+    toggleMutation.mutate({ id, field: 'isActive', value: !currentValue });
+  };
+
+  const products = data?.products ?? [];
 
   return (
     <div className="max-w-7xl">
@@ -56,7 +147,7 @@ export default function ProductsPage() {
 
       {/* Filters */}
       <div className="flex items-center gap-3 mb-6">
-        <div className="flex-1 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-3">
+        <div className="flex-1 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-3">
           <Select
             value={filters.category}
             onChange={(e) => setFilters({ ...filters, category: e.target.value })}
@@ -74,33 +165,14 @@ export default function ProductsPage() {
           />
 
           <Select
-            value={filters.diveType}
-            onChange={(e) => setFilters({ ...filters, diveType: e.target.value })}
-            options={diveTypes}
-            placeholder="Semua Tipe"
-            inline
-          />
-
-          <Select
             value={filters.status}
             onChange={(e) => setFilters({ ...filters, status: e.target.value })}
             placeholder="Semua Status"
             inline
           >
             <option value="">Semua Status</option>
-            <option value="active">Aktif</option>
-            <option value="inactive">Nonaktif</option>
-          </Select>
-
-          <Select
-            value={filters.stock}
-            onChange={(e) => setFilters({ ...filters, stock: e.target.value })}
-            placeholder="Semua Stok"
-            inline
-          >
-            <option value="">Semua Stok</option>
-            <option value="in_stock">Tersedia</option>
-            <option value="out_of_stock">Habis</option>
+            <option value="true">Aktif</option>
+            <option value="false">Nonaktif</option>
           </Select>
         </div>
         {activeFilterCount > 0 && (
@@ -113,96 +185,129 @@ export default function ProductsPage() {
         )}
       </div>
 
+      {/* Loading State */}
+      {isLoading && (
+        <div className="text-center py-16">
+          <div className="w-16 h-16 rounded-2xl bg-neutral-100 flex items-center justify-center mx-auto mb-4 animate-pulse">
+            <Icon icon="solar:box-linear" className="w-8 h-8 text-neutral-400" />
+          </div>
+          <p className="text-neutral-500">Memuat produk...</p>
+        </div>
+      )}
+
+      {/* Error State */}
+      {error && (
+        <div className="text-center py-16">
+          <p className="text-red-600">Gagal memuat produk. Silakan coba lagi.</p>
+        </div>
+      )}
+
       {/* Product List */}
-      <div className="space-y-2">
-        {products.map((product) => (
-          <Link
-            key={product.id}
-            href={`/admin/products/${product.id}`}
-            className="flex items-center gap-4 p-4 bg-white rounded-xl hover:bg-neutral-50 transition-colors group"
-          >
-            {/* Product Image */}
-            <div className="w-16 h-16 sm:w-20 sm:h-20 rounded-lg bg-neutral-100 flex-shrink-0 flex items-center justify-center text-neutral-900">
-              <Icon icon="solar:gallery-minimalistic-linear" className="w-6 h-6" />
-            </div>
-
-            {/* Product Info */}
-            <div className="flex-1 min-w-0">
-              <div className="flex items-center gap-2 mb-1 flex-wrap">
-                <span className="text-[10px] uppercase tracking-widest text-neutral-500">
-                  {getLabelById(categories, product.category)}
-                </span>
-                <span className="text-[10px] text-neutral-300">•</span>
-                <span className="text-[10px] uppercase tracking-widest text-neutral-400">
-                  {getLabelById(brands, product.brand)}
-                </span>
-                <span className="text-[10px] text-neutral-300">•</span>
-                <span className="text-[10px] uppercase tracking-widest text-neutral-400">
-                  {getLabelById(diveTypes, product.diveType)}
-                </span>
-                {!product.isActive && (
-                  <span className="px-2 py-0.5 text-[10px] uppercase tracking-wider bg-neutral-200 text-neutral-600 rounded-full">
-                    Nonaktif
-                  </span>
+      {!isLoading && !error && (
+        <div className="space-y-2">
+          {products.map((product) => (
+            <Link
+              key={product.id}
+              href={`/admin/products/${product.id}`}
+              className="flex items-center gap-4 p-4 bg-white rounded-xl hover:bg-neutral-50 transition-colors group"
+            >
+              {/* Product Image */}
+              <div className="w-16 h-16 sm:w-20 sm:h-20 rounded-lg bg-neutral-100 flex-shrink-0 overflow-hidden">
+                {product.images[0] ? (
+                  <img src={product.images[0]} alt={product.name} className="w-full h-full object-cover" />
+                ) : (
+                  <div className="w-full h-full flex items-center justify-center text-neutral-400">
+                    <Icon icon="solar:gallery-minimalistic-linear" className="w-6 h-6" />
+                  </div>
                 )}
               </div>
-              <h3 className="text-base font-medium tracking-tight text-neutral-900 truncate">
-                {product.title}
-              </h3>
-              <div className="flex items-baseline gap-2 mt-1">
-                <span className="text-sm text-neutral-700">
-                  {product.price}
-                </span>
-                {product.compareAtPrice && (
-                  <span className="text-sm text-neutral-400 line-through">
-                    {product.compareAtPrice}
+
+              {/* Product Info */}
+              <div className="flex-1 min-w-0">
+                <div className="flex items-center gap-2 mb-1 flex-wrap">
+                  <span className="text-[10px] uppercase tracking-widest text-neutral-500">
+                    {product.category?.name ?? '-'}
                   </span>
-                )}
+                  {product.brand && (
+                    <>
+                      <span className="text-[10px] text-neutral-300">•</span>
+                      <span className="text-[10px] uppercase tracking-widest text-neutral-400">
+                        {product.brand.name}
+                      </span>
+                    </>
+                  )}
+                  {!product.isActive && (
+                    <span className="px-2 py-0.5 text-[10px] uppercase tracking-wider bg-neutral-200 text-neutral-600 rounded-full">
+                      Nonaktif
+                    </span>
+                  )}
+                </div>
+                <h3 className="text-base font-medium tracking-tight text-neutral-900 truncate">
+                  {product.name}
+                </h3>
+                <div className="flex items-baseline gap-2 mt-1">
+                  <span className="text-sm text-neutral-700">
+                    {formatPrice(product.priceCents)}
+                  </span>
+                  {product.compareAtPriceCents && (
+                    <span className="text-sm text-neutral-400 line-through">
+                      {formatPrice(product.compareAtPriceCents)}
+                    </span>
+                  )}
+                </div>
               </div>
-            </div>
 
-            {/* Availability & Actions */}
-            <div className="hidden sm:flex items-center gap-6">
-              <div className="text-right">
-                <p className="text-xs text-neutral-500 mb-0.5">Ketersediaan</p>
-                <p className={`text-sm ${
-                  product.stockStatus === 'in_stock' ? 'text-green-600' : 'text-red-600'
-                }`}>
-                  {product.stockStatus === 'in_stock' ? 'Tersedia' : 'Habis'}
-                </p>
+              {/* Availability & Actions */}
+              <div className="hidden sm:flex items-center gap-6">
+                <div className="text-right">
+                  <p className="text-xs text-neutral-500 mb-0.5">Status</p>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleToggleActive(product.id, product.isActive);
+                    }}
+                    className={`text-sm px-2 py-0.5 rounded transition-colors ${
+                      product.isActive 
+                        ? 'text-green-600 hover:bg-green-50' 
+                        : 'text-neutral-500 hover:bg-neutral-100'
+                    }`}
+                  >
+                    {product.isActive ? 'Aktif' : 'Nonaktif'}
+                  </button>
+                </div>
+                <div className="flex gap-1">
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      // Navigate to edit page (handled by link)
+                    }}
+                    className="w-9 h-9 rounded-lg flex items-center justify-center text-neutral-900 hover:bg-neutral-100 transition-colors"
+                    aria-label="Edit produk"
+                  >
+                    <Icon icon="solar:pen-linear" className="w-4 h-4" />
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleDelete(product.id);
+                    }}
+                    className="w-9 h-9 rounded-lg flex items-center justify-center text-neutral-900 hover:text-red-600 hover:bg-red-50 transition-colors"
+                    aria-label="Hapus produk"
+                  >
+                    <Icon icon="solar:trash-bin-minimalistic-linear" className="w-4 h-4" />
+                  </button>
+                </div>
               </div>
-              <div className="flex gap-1">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    // Handle edit
-                  }}
-                  className="w-9 h-9 rounded-lg flex items-center justify-center text-neutral-900 hover:bg-neutral-100 transition-colors"
-                  aria-label="Edit produk"
-                >
-                  <Icon icon="solar:pen-linear" className="w-4 h-4" />
-                </button>
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    // Handle delete
-                  }}
-                  className="w-9 h-9 rounded-lg flex items-center justify-center text-neutral-900 hover:text-red-600 hover:bg-red-50 transition-colors"
-                  aria-label="Hapus produk"
-                >
-                  <Icon icon="solar:trash-bin-minimalistic-linear" className="w-4 h-4" />
-                </button>
-              </div>
-            </div>
 
-            {/* Mobile Arrow */}
-            <Icon icon="solar:alt-arrow-right-linear" className="w-5 h-5 text-neutral-900 flex-shrink-0 sm:hidden" />
-          </Link>
-        ))}
-      </div>
+              {/* Mobile Arrow */}
+              <Icon icon="solar:alt-arrow-right-linear" className="w-5 h-5 text-neutral-900 flex-shrink-0 sm:hidden" />
+            </Link>
+          ))}
+        </div>
+      )}
 
       {/* Empty State */}
-      {products.length === 0 && (
+      {!isLoading && !error && products.length === 0 && (
         <div className="text-center py-16">
           <div className="w-16 h-16 rounded-2xl bg-neutral-100 flex items-center justify-center mx-auto mb-4">
             <Icon icon="solar:box-linear" className="w-8 h-8 text-neutral-400" />
